50fd8873bf669b10c01daed842ce54e0718893d0
Emre Özel <emreozel.36@hotmail.com>
2022-11-30 16:49:13 +0300

    Create user

--- app/src/main/java/com/mte/fitnessapp/LoginActivity.kt
+++ app/src/main/java/com/mte/fitnessapp/LoginActivity.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.mte.fitnessapp.databinding.ActivityLoginBinding
 import com.mte.fitnessapp.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
-
+    lateinit var auth: FirebaseAuth
     private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +22,36 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
+        auth=FirebaseAuth.getInstance()
+        // kullanıcı girip yaptığını kontrol ediyorum
+        var currentUser=auth.currentUser
         binding.loginBtn.setOnClickListener {
+            var girisemail=binding.email.text.toString()
+            var girissifre=binding.password.text.toString()
+            if(TextUtils.isEmpty(girisemail)){
+                binding.email.error="Lütfen email adresinizi giriniz"
+                return@setOnClickListener
+            }else if(TextUtils.isEmpty(girissifre)){
+                binding.password.error="Lütfen şifrenizi giriniz"
+                return@setOnClickListener
+            }
+            //giris bilgilerini doğrulama
+            auth.signInWithEmailAndPassword(girisemail,girissifre)
+                .addOnCompleteListener(this) {
+                    if(it.isSuccessful){
+                        val verification = auth.currentUser?.isEmailVerified
+                        if(verification==true) {
+                            intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else{
 
+                            Toast.makeText(this,"Öncelikle Mail Adresinizi Doğrulamalısınız", Toast.LENGTH_LONG).show()
+                        }
+                    }else{
+                        Toast.makeText(applicationContext,"Giriş hatalı tekrar deneyiniz", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
         binding.registerBtn.setOnClickListener {
